be2af6c68e56ad69fc5edea176c6ca4ff7702584
acailly <ylliac14@gmail.com>
2019-02-18 21:41:05 +0100

    Set a basename to make static deploy work

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Identite from "./Identite";
-import {BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Associations from "./Associations";
 import Scanner from "./Scanner";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/scan-and-give">
         <div>
-          <Route exact path="/" component={Associations}/>
-          <Route path="/scanner" component={Scanner}/>
-          <Route path="/identite" component={Identite}/>
+          <Route exact path="/" component={Associations} />
+          <Route path="/scanner" component={Scanner} />
+          <Route path="/identite" component={Identite} />
         </div>
       </BrowserRouter>
-    )
+    );
   }
 }
 
