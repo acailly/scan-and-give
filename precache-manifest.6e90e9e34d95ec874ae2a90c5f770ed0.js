187f038428d2441d241e6882b8d5d392cc7e9fcd
acailly <ylliac14@gmail.com>
2019-02-18 21:37:33 +0100

    Updates

--- precache-manifest.6e90e9e34d95ec874ae2a90c5f770ed0.js
+++ precache-manifest.6e90e9e34d95ec874ae2a90c5f770ed0.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/scan-and-give/static/js/runtime~main.50c8b04a.js"
   },
   {
-    "revision": "6819ac2e57dcba260d08",
-    "url": "/scan-and-give/static/js/main.6819ac2e.chunk.js"
+    "revision": "efff663539ec4140afb7",
+    "url": "/scan-and-give/static/js/main.efff6635.chunk.js"
   },
   {
     "revision": "c9ebb966bc1d64aa0b42",
     "url": "/scan-and-give/static/js/1.c9ebb966.chunk.js"
   },
   {
-    "revision": "6819ac2e57dcba260d08",
+    "revision": "efff663539ec4140afb7",
     "url": "/scan-and-give/static/css/main.e03829f7.chunk.css"
   },
   {
-    "revision": "202a70bdb59285213ca7a8700523d309",
+    "revision": "0fcba6866cad994268bc9f5939a8f9b3",
     "url": "/scan-and-give/index.html"
   }
 ];
