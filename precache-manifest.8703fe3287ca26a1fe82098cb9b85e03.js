face9e326a0c24ab9d573de4595c92c5b764d67e
acailly <ylliac14@gmail.com>
2019-02-18 21:31:32 +0100

    Updates

--- precache-manifest.8703fe3287ca26a1fe82098cb9b85e03.js
+++ precache-manifest.8703fe3287ca26a1fe82098cb9b85e03.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/scan-and-give/static/js/runtime~main.50c8b04a.js"
   },
   {
-    "revision": "6819ac2e57dcba260d08",
-    "url": "/scan-and-give/static/js/main.6819ac2e.chunk.js"
+    "revision": "3c4fd1e0f2aa2c82bc7f",
+    "url": "/scan-and-give/static/js/main.3c4fd1e0.chunk.js"
   },
   {
     "revision": "c9ebb966bc1d64aa0b42",
     "url": "/scan-and-give/static/js/1.c9ebb966.chunk.js"
   },
   {
-    "revision": "6819ac2e57dcba260d08",
+    "revision": "3c4fd1e0f2aa2c82bc7f",
     "url": "/scan-and-give/static/css/main.e03829f7.chunk.css"
   },
   {
-    "revision": "202a70bdb59285213ca7a8700523d309",
+    "revision": "adec1f78c11ee32d7b93fddf68cf5c1d",
     "url": "/scan-and-give/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/scan-and-give/precache-manifest.3ccaf4c1ca2505e755706cbde7d33de2.js"
+  "/scan-and-give/precache-manifest.8703fe3287ca26a1fe82098cb9b85e03.js"
 );
 
 workbox.clientsClaim();
