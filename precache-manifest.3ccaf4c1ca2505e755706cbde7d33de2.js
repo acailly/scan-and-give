1eccd81220a1fec0708bfb76e3ca26d40d4437f5
acailly <ylliac14@gmail.com>
2019-02-18 21:21:58 +0100

    Updates

---
+++ precache-manifest.3ccaf4c1ca2505e755706cbde7d33de2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50c8b04a56562be30ebb",
+    "url": "/scan-and-give/static/js/runtime~main.50c8b04a.js"
+  },
+  {
+    "revision": "6819ac2e57dcba260d08",
+    "url": "/scan-and-give/static/js/main.6819ac2e.chunk.js"
+  },
+  {
+    "revision": "c9ebb966bc1d64aa0b42",
+    "url": "/scan-and-give/static/js/1.c9ebb966.chunk.js"
+  },
+  {
+    "revision": "6819ac2e57dcba260d08",
+    "url": "/scan-and-give/static/css/main.e03829f7.chunk.css"
+  },
+  {
+    "revision": "202a70bdb59285213ca7a8700523d309",
+    "url": "/scan-and-give/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/scan-and-give/precache-manifest.96f4620406d611ba0922d9457f767008.js"
+  "/scan-and-give/precache-manifest.3ccaf4c1ca2505e755706cbde7d33de2.js"
 );
 
 workbox.clientsClaim();
