c85436bf9be59b66279058b3ecf62be2b07d9344
acailly <ylliac14@gmail.com>
2019-02-25 22:25:09 +0100

    :sparkles: Récupération du nombre de dons

--- server/app.js
+++ server/app.js
@@ -1,39 +1,64 @@
-const express = require('express');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const bodyParser = require('body-parser');
+const express = require("express");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const bodyParser = require("body-parser");
 const app = express();
-const adapter = new FileSync('db.json');
+const adapter = new FileSync("db.json");
 const db = low(adapter);
 app.use(bodyParser.json());
 
-app.get('/api/associations', function (req, res) {
-  const associations = db.get('associations').value();
-  res.send(associations)
+app.get("/api/associations", function(req, res) {
+  const associations = db.get("associations").value();
+  res.send(associations);
 });
 
-app.get('/api/associations/:associationId', function (req, res) {
-  const association = db.get('associations')
-    .find({id: parseInt(req.params.associationId)})
+app.get("/api/associations/:associationId", function(req, res) {
+  const association = db
+    .get("associations")
+    .find({ id: parseInt(req.params.associationId) })
     .value();
-  res.send(association)
+  res.send(association);
 });
 
-app.post('/api/dons', function (req, res) {
-  const hasDon = db.get('dons')
-    .find({hash: req.body.hash})
+app.post("/api/dons", function(req, res) {
+  const hasDon = db
+    .get("dons")
+    .find({ hash: req.body.hash })
     .value();
   let don = null;
   if (hasDon === undefined) {
-    don = db.get('dons')
+    don = db
+      .get("dons")
       .push(req.body)
       .write();
   } else {
-    res.status(400)
+    res.status(400);
   }
   res.send(don);
 });
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!')
+app.get("/api/dons/:associationId", function(req, res) {
+  const associationId = req.params.associationId;
+  const dons = db
+    .get("dons")
+    .filter({ associationId: parseInt(associationId) })
+    .size()
+    .value();
+  res.send({ associationId, dons });
+});
+
+app.get("/api/dons", function(req, res) {
+  const donsByAssociationId = db
+    .get("dons")
+    .groupBy("associationId")
+    .map((dons, associationId) => ({
+      associationId,
+      dons: dons.length
+    }))
+    .value();
+  res.send(donsByAssociationId);
+});
+
+app.listen(3001, function() {
+  console.log("Example app listening on port 3001!");
 });
