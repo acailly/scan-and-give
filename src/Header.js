3c308235b40198a16602880d3ec82ceb578d4fed
acailly <ylliac14@gmail.com>
2019-03-05 22:12:52 +0100

    :hammer: On peut revenir sur la liste des assos grace au bouton du header

--- src/Header.js
+++ src/Header.js
@@ -1,40 +1,42 @@
-import {Component} from "react";
-import {withStyles} from "@material-ui/core";
-import {Link} from "react-router-dom";
+import { Component } from "react";
+import { withStyles } from "@material-ui/core";
+import { Link, withRouter } from "react-router-dom";
 import React from "react";
 import Icon from "@material-ui/core/Icon";
 import Button from "@material-ui/core/Button";
 
 const styles = () => ({
-    zenikanard: {
-      height: 180,
-      marginRight: 40
-    },
-    header: {
-      fontSize: 64,
-      display: 'flex',
-      alignItems: 'center',
-      backgroundColor: '#2b2c2c',
-    },
-    title: {
-      flexGrow: 2,
-      color: '#4e7b53',
-      textShadow: '0 0 5px #fff, 0 0 10px #fff, 0 0 20px #4e7b53, 0 0 30px #4e7b53, 0 0 40px #4e7b53, 0 0 55px #4e7b53, 0 0 75px #4e7b53',
-    },
-    button: {
-      color: '#c31436'
-    },
-  })
-;
-
+  zenikanard: {
+    height: 180,
+    marginRight: 40
+  },
+  header: {
+    fontSize: 64,
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "#2b2c2c"
+  },
+  title: {
+    flexGrow: 2,
+    color: "#4e7b53",
+    textShadow:
+      "0 0 5px #fff, 0 0 10px #fff, 0 0 20px #4e7b53, 0 0 30px #4e7b53, 0 0 40px #4e7b53, 0 0 55px #4e7b53, 0 0 75px #4e7b53"
+  },
+  button: {
+    color: "#c31436"
+  }
+});
 class Header extends Component {
-
   render() {
-    const {classes} = this.props;
+    const { classes, location } = this.props;
     return (
       <div className={classes.header}>
         <Link to="/">
-          <img src="/zenikanard2.png" alt="zenikanard" className={classes.zenikanard}/>
+          <img
+            src="/zenikanard2.png"
+            alt="zenikanard"
+            className={classes.zenikanard}
+          />
         </Link>
         <span className={classes.title}>Breizh Give</span>
         <div>
@@ -43,16 +45,14 @@ class Header extends Component {
             size="large"
             color="primary"
             component={Link}
-            to={"/dons"}
+            to={location.pathname === "/dons" ? "/" : "/dons"}
           >
-            <Icon>
-              bar_chart
-            </Icon>
+            <Icon>bar_chart</Icon>
           </Button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default withStyles(styles)(Header);
+export default withStyles(styles)(withRouter(Header));
