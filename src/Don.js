c2964e7d53b78b63245cf8744b90d5991c136d97
acailly <ylliac14@gmail.com>
2019-03-08 21:12:19 +0100

    :lipstick: Mise à jour du style de la page des dons

--- src/Don.js
+++ src/Don.js
@@ -1,52 +1,71 @@
 import React, { Component } from "react";
-import CardMedia from "@material-ui/core/CardMedia";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import { withStyles } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 
 const styles = () => ({
+  titre: {
+    color: "#00a94e"
+  },
+  content: {
+    paddingRight: 30,
+    color: "#333"
+  },
+  cardImageContainer: {
+    flex: "0 0 330px",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center"
+  },
   card: {
+    borderRadius: 10,
+    width: "100%",
     display: "flex",
     marginBottom: 15,
     minHeight: 200,
-    backgroundColor: '#afb0af'
+    backgroundColor: "white",
+    fontFamily: "Ghostbusters",
+    fontStyle: "normal",
+    fontWeight: 400
   },
   cardImage: {
-    height: 180,
-    width: 450,
-    backgroundSize: "contain",
-    alignSelf: "center"
+    maxWidth: 300,
+    height: 180
   },
   cardContent: {
-    display: "flex",
-    flexDirection: "column",
-    flexGrow: 1,
-    justifyContent: "center"
+    flexGrow: 1
   },
   rightIcon: {
-    marginLeft: 8
+    marginLeft: 8,
+    color: "#C00"
+  },
+  button: {
+    color: "#C00"
+  },
+  textButton: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   }
 });
 class Don extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Card className={classes.card}>
-        <CardMedia
-          className={classes.cardImage}
-          image={this.props.image}
-          title={this.props.association}
-        />
-        <CardContent className={classes.cardContent}>
-          <Typography gutterBottom variant="h5" component="h2">
-            {this.props.association}
-          </Typography>
-          <Typography variant="h2" component="h1">
+      <div className={classes.card}>
+        <div className={classes.cardImageContainer}>
+          <img
+            className={classes.cardImage}
+            src={this.props.image}
+            alt={this.props.association}
+          />
+        </div>
+        <div className={classes.cardContent}>
+          <h2 className={classes.titre}>{this.props.association}</h2>
+          <Typography variant="h2" component="h1" className={classes.content}>
             {this.props.nombre}€ grâce à vous !
           </Typography>
-        </CardContent>
-      </Card>
+        </div>
+      </div>
     );
   }
 }
