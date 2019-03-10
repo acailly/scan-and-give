626f01dcd8a7b713425026ab5cfa77d7452c6cb8
acailly <ylliac14@gmail.com>
2019-03-10 14:49:00 +0100

    :lipstick: Augmente la taille de la police utilisée pour la description

--- src/Association.js
+++ src/Association.js
@@ -1,68 +1,61 @@
-import React, {Component} from "react";
-import {withStyles} from "@material-ui/core";
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import ButtonBase from "@material-ui/core/ButtonBase";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const styles = () => ({
-    titre: {
-      color: "#00a94e"
-    },
-    content: {
-      paddingRight: 30
-    },
-    cardImageContainer: {
-      flex: '1 0 330px',
-      alignItems: 'center',
-      display: 'flex',
-      justifyContent: 'center'
-    },
-    card: {
-      borderRadius: 10,
-      width: '100%',
-      display: "flex",
-      marginBottom: 15,
-      minHeight: 200,
-      backgroundColor: "white",
-      fontFamily: "Ghostbusters",
-      fontStyle: "normal",
-      fontWeight: 400
-    },
-    cardImage: {
-      maxWidth: 300,
-      height: 180,
-    },
-    cardContent: {
-      flexGrow: 1
-    }
-    ,
-    rightIcon: {
-      marginLeft: 8,
-      color:
-        "#C00"
-    }
-    ,
-    button: {
-      color: "#C00"
-    }
-    ,
-    textButton: {
-      display: "flex",
-      flexDirection:
-        "column",
-      alignItems:
-        "center"
-    }
-  })
-;
-
+  titre: {
+    color: "#00a94e"
+  },
+  content: {
+    paddingRight: 30,
+    fontSize: "18px"
+  },
+  cardImageContainer: {
+    flex: "1 0 330px",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center"
+  },
+  card: {
+    borderRadius: 10,
+    width: "100%",
+    display: "flex",
+    marginBottom: 15,
+    minHeight: 200,
+    backgroundColor: "white",
+    fontFamily: "Ghostbusters",
+    fontStyle: "normal",
+    fontWeight: 400
+  },
+  cardImage: {
+    maxWidth: 300,
+    height: 180
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  rightIcon: {
+    marginLeft: 8,
+    color: "#C00"
+  },
+  button: {
+    color: "#C00"
+  },
+  textButton: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  }
+});
 class Association extends Component {
   state = {
     associations: []
   };
 
   render() {
-    const {classes} = this.props;
+    const { classes } = this.props;
     return (
       <ButtonBase focusRipple component={Link} to={"/scanner/" + this.props.id}>
         <div className={classes.card}>
