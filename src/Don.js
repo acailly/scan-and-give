24242089cf5991375a07d771fa0fa81edda9f7d9
acailly <ylliac14@gmail.com>
2019-03-12 21:37:19 +0100

    :lipstick: Reorganisation des cartes sur la page de dons

--- src/Don.js
+++ src/Don.js
@@ -5,15 +5,16 @@ import Typography from "@material-ui/core/Typography";
 const styles = () => ({
   titre: {
     color: "#00a94e",
-    textAlign: 'center'
+    textAlign: "center"
   },
   content: {
     paddingRight: 30,
     color: "#333",
-    marginBottom: "20px"
+    marginBottom: "20px",
+    textAlign: "center"
   },
   cardImageContainer: {
-    flex: "0 0 30%",
+    flex: "0 0 100px",
     alignItems: "center",
     display: "flex",
     justifyContent: "center"
@@ -22,6 +23,7 @@ const styles = () => ({
     borderRadius: 10,
     width: "100%",
     display: "flex",
+    flexDirection: "column",
     minHeight: 170,
     backgroundColor: "white",
     fontFamily: "Ghostbusters",
@@ -29,11 +31,17 @@ const styles = () => ({
     fontWeight: 400
   },
   cardImage: {
-    maxWidth: 300,
-    height: 120
+    maxWidth: "100%",
+    maxHeight: "100%",
+    height: "100px",
+    flex: "0 1 0%"
   },
   cardContent: {
-    flexGrow: 1
+    flexGrow: 1,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
   },
   rightIcon: {
     marginLeft: 8,
@@ -51,12 +59,17 @@ const styles = () => ({
 class Don extends Component {
   render() {
     const { classes } = this.props;
-    let nombre = Math.floor(this.props.nombre / this.props.nombre_correspondance);
-    let correspondance = this.props.correspondance && nombre >= 1 ? (
-      <Typography variant="h4" component="h1" className={classes.content}>
-        soit {nombre} {this.props.correspondance}
-      </Typography>
-    ) : '';
+    let nombre = Math.floor(
+      this.props.nombre / this.props.nombre_correspondance
+    );
+    let correspondance =
+      this.props.correspondance && nombre >= 1 ? (
+        <Typography variant="h4" component="h1" className={classes.content}>
+          soit {nombre} {this.props.correspondance}
+        </Typography>
+      ) : (
+        ""
+      );
     return (
       <div className={classes.card}>
         <div className={classes.cardImageContainer}>
@@ -65,9 +78,9 @@ class Don extends Component {
             src={this.props.image}
             alt={this.props.association}
           />
+          <h2 className={classes.titre}>{this.props.association}</h2>
         </div>
         <div className={classes.cardContent}>
-          <h2 className={classes.titre}>{this.props.association}</h2>
           <Typography variant="h4" component="h1" className={classes.content}>
             {this.props.nombre}€ reçu
           </Typography>
